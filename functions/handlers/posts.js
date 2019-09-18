f4b4002a75ac5634f92dda6efbbda6a281cacf8c
Ferninad <22896731+Ferninad@users.noreply.github.com>
2019-09-18 14:16:42 -0500

    like a comment

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -104,7 +104,44 @@ exports.commentOnPost = (req, res) => {
 };
 
 exports.likePost = (req, res) => {
+    const likeDocument = db.collection('likes').where('userHandle', '==', req.user.handle)
+        .where('postId', '==', req.params.postId).limit(1);
+
+    const postDocument = db.doc(`/posts/${req.params.postId}`);
     
+    let postData;
+
+    postDocument.get()
+        .then(doc => {
+            if(doc.exists){
+                postData = doc.data();
+                postData.postId = doc.id;
+                return likeDocument.get();
+            } else{
+                return res.stauts(404).json({ error: 'post not found'})
+            }
+        })
+        .then(data => {
+            if(data.empty){
+                return db.collection('likes').add({
+                    postId: req.params.postId,
+                    userHandle: req.user.handle
+                })
+                .then(() => {
+                    postData.likeCount++;
+                    return postDocument.update({ likeCount: postData.likeCount });
+                })
+                .then(() => {
+                    return res.json(postData);
+                })
+            } else {
+                return res.status(400).json({ error: 'Post already liked'})
+            }
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(500).json({ error: err.code})
+        })
 };
 
 exports.unlikePost = (req, res) => {
